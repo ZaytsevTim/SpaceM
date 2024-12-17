5dd32781a67e5de84832ca67ebc8084217dc5ff8
ZaytsevTim <159259376+ZaytsevTim@users.noreply.github.com>
2024-12-17 22:08:07 +0300

    Update Krpc plot Change Orbit.py

--- krpc-plots/Krpc plot Change Orbit.py
+++ krpc-plots/Krpc plot Change Orbit.py
@@ -1,5 +1,6 @@
 import krpc
 import time
+import csv
 import matplotlib.pyplot as plt
 def main():
     # Подключение к серверу kRPC
@@ -11,29 +12,34 @@ def main():
     #получение информации о небесном теле - Луне
     body = vessel.orbit.body 
 
+    #Открытие csv файла
+    w_file = open("KSP change orbit data.csv", mode="w", encoding="UTF-8")
+    fieldnames = ['Time since maneuver start', 'Ingame Time', 'Velocity']
+    writer = csv.DictWriter(w_file, fieldnames=fieldnames)
+    writer.writeheader()   
     # Инициализация списков для хранения данных
     velocity_data = []
     time_data = [] 
 
     try:
-        # Ждем, пока Луна-25 не приблизится к апоцентру
-        while vessel.orbit.time_to_apoapsis > 1.5:
-          print(vessel.orbit.time_to_apoapsis)
+        # Ждем, пока не зарабоатет двигатель Луны-25 
+        while vessel.control.throttle == 0:
+          print(conn.space_center.ut, vessel.control.throttle)
           time.sleep(0.5)
-        # Маневр начался. Т.к измерения каждые 0.5 сек (двигатель работает около 84 сек), считываем данные, пока k < 169 
+        # Маневр начался. Считываем данные, пока работает двигатель
         k = 0      
-        while k < (84 * 2 + 1):  
+        while vessel.control.throttle > 0:  
           # Получение текущего  времени
-          cur_time = conn.space_center.ut
+          cur_time = round(conn.space_center.ut, 2)
           # Получение текущей скорости
-          velocity = vessel.flight(body.orbital_reference_frame).speed
+          velocity = round(vessel.flight(body.orbital_reference_frame).speed, 5)
           # Запись данных
           velocity_data.append(velocity)
-          time_data.append(cur_time)
-          print(k, cur_time,velocity, vessel.orbit.time_to_apoapsis)
+          time_data.append(round(cur_time, 2))
+          print(k, cur_time,velocity, vessel.orbit.time_to_apoapsis, vessel.control.throttle)
           k += 1
           # Задержка перед следующим измерением
-          time.sleep(0.5) 
+          time.sleep(1) 
 
 
     except KeyboardInterrupt:
@@ -42,6 +48,10 @@ def main():
     finally:
         # Закрытие соединения с сервером kRPC
         conn.close()
+        #Запись полученных данных в csv файл, закрытие файла
+        for n in range(len(time_data)):
+            writer.writerow({'Time since maneuver start': n, 'Ingame Time': time_data[n], 'Velocity': velocity_data[n]})
+        w_file.close()
         # Построение графика зависимости скорости от времени 
         plt.plot(time_data, velocity_data)
         plt.xlabel('Время (секунды)')
